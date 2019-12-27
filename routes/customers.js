7043c6af4409f10efa1078fc3a358d5ce1d63812
Alexis Mendez <alexmendiesel@Alexiss-MBP.fios-router.home>
2019-12-26 20:58:15 -0500

    updated

--- routes/customers.js
+++ routes/customers.js
@@ -37,9 +37,26 @@ router.post('/', async (req, res) => {
       name: req.body.name 
       phone: req.body.phone
       isGold req.body.isGold}); 
-  await customer.save();
+  customer = await customer.save();
 
   res.send(customer);
+});
+
+// PUT 
+router.put('/:id', async (req, res) => {
+  const { error } = validateCustomer(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+  
+  const customer = await Customer.findByIdAndUpdate(req.params.id, 
+  { 
+    name: req.body.name, 
+    isGold: req.body.isGold, 
+    phone: req.body.phone
+  }, {new: true}); 
+
+  if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+
+  res.send(genre);
 });
 
 // DELETE 
@@ -59,6 +76,7 @@ router.get('/:id', async (req, res) => {
   res.send(customer);
 });
 
+// Validate customer function
 function validateCustomer(customer) {
   const schema = {
     name: Joi.string().min(5).max(50)required(),
